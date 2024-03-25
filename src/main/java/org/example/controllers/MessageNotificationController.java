afc5cd8cc2530270ec30191104287beca1f73695
Easy Soft <Administrator@MAY02.MAY20>
2024-03-25 20:26:10 +0700

    update

--- src/main/java/org/example/controllers/MessageNotificationController.java
+++ src/main/java/org/example/controllers/MessageNotificationController.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -39,7 +40,7 @@ public class MessageNotificationController {
 
 
     @GetMapping("/conversations/{conversationId}/unreadMessages/user/{userId}")
-    public ResponseEntity<ResponseDTO<List<MessageNotificationDTO>>> findUnreadMessagesByUserIdAndConversationId(@DestinationVariable Long conversationId, @DestinationVariable Long userId) {
+    public ResponseEntity<ResponseDTO<List<MessageNotificationDTO>>> findUnreadMessagesByUserIdAndConversationId(@PathVariable Long conversationId, @PathVariable Long userId) {
         return ResponseEntity.ok(this.messageNotificationService.findUnreadMessagesByConversationIdAndUserId(conversationId, userId));
     }
 }
